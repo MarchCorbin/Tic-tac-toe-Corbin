1f7be09c81a167a8eaa36af557c421007631c5c6
Corbin March <marchcorbin@gmail.com>
2020-04-21 18:41:33 -0600

    board population under work

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var gameBoard = document.querySelector('.game-board')
-var player1 = new Player('O')
-var player2 = new Player('X')
+var player1 = new Player('X', 'player1')
+var player2 = new Player('O', 'player2')
 var freshGame = new Game(player1, player2)
 var player1Board = document.querySelector('.player1-boards')
 var player2Board = document.querySelector('.player2-boards')
@@ -8,10 +8,10 @@ var player2Board = document.querySelector('.player2-boards')
 gameBoard.addEventListener('click',function(){
 drawBoard()
 getID()
-// drawPlayerBoards()
+reinstate()
 });
-
-
+var player1Stored = []
+var player2Stored = []
 function drawBoard(){
   for (var i = 0; i < freshGame.board.length; i++) {
     for (var j = 0; j < freshGame.board[i].length; j++) {
@@ -22,13 +22,62 @@ function drawBoard(){
         gotYou.innerHTML = `<p>${freshGame.board[i][j]}</p>`
         }
       }
+      }
     }
+//
+function drawPlayerBoards(){
+  if(freshGame.winner === 'player1'){
   }
+for (var i = 0; i < player1Stored.length; i++) {
+      var slotValue = player1Stored[i]
+      if (slotValue === undefined){
+        slotValue = '-'
+      } else {
+        slotValue = slotValue
+      }
+console.log(slotValue);
+    var player1Boards = `
+      <section class='small-tictac-board'>
+      <article class="srow1">
+      <div class="scol col1" id='p0-0'>${slotValue[0][0]}</div>
+      <div class="scol col2" id='p0-1'>${slotValue[0][1]}</div>
+      <div class="scol col3" id='p0-2'>${slotValue[0][2]}</div>
+    </article>
+    <article class="srow2">
+      <div class="scol col4" id='p1-0'>${slotValue[1][0]}</div>
+      <div class="scol col5" id='p1-1'>${slotValue[1][1]}</div>
+      <div class="scol col6" id='p1-2'>${slotValue[1][2]}</div>
+    </article>
+    <article class="srow3">
+      <div class="scol col7" id='p2-0'>${slotValue[2][0]}</div>
+      <div class="scol col8" id='p2-1'>${slotValue[2][1]}</div>
+      <div class="scol col9" id='p2-2'>${slotValue[2][2]}</div>
+    </article>
+    </section>`
+    player1Board.insertAdjacentHTML('afterBegin', player1Boards)
 
-// drawPlayerBoards(){
+}
+}
+
+
+// loop through savedGames twice when nested target the section where we want the board to go on the corresponding side of the board using insertAdjacentHTML.
+
+
 
 // }
-
+function reinstate(){
+  if(freshGame.winner === 'player1'){
+  var savedGames = player1.retrieveWins()
+  var storeTheBoard = savedGames[0]
+  player1Stored.push(storeTheBoard)
+  drawPlayerBoards()
+} else if(freshGame.winner === 'player2'){
+  var savedGames = player2.retrieveWins()
+  var storeTheBoard = savedGames[0]
+  player2Stored.push(storeTheBoard)
+  drawPlayerBoards()
+}
+}
 
 
 
